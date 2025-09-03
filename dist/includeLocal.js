b317b57c6030f8be84d3539248503438d628bf76
Sherpad Ndabambi <sgndabambi@gmail.com>
2025-09-03 23:34:12 +0200

    Delete dist

--- dist/includeLocal.js
+++
@@ -1,6 +0,0 @@
-function includeLocalCSS(fileName) {
-    document.write(`<link href='css/vendor/${fileName}' rel="stylesheet">`);
-}
-function includeLocalJS(fileName) {
-    document.write(`<script type='text/javascript' src='js/vendor/${fileName}'><\/script>`);
-}
